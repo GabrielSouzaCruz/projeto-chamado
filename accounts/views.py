7d8c0da7c9ea22b8d331f92e0ead35a7fa29286c
Gabriel de Souza Cruz <12989644660@campus.machado.mch>
2026-03-02 10:52:01 -0300

    feat: tudo funcionando

--- accounts/views.py
+++ accounts/views.py
@@ -1,240 +1,148 @@
-# accounts/views.py
+# accounts/views.py - ARQUIVO CORRIGIDO
+# Substitua todo o conteúdo do seu accounts/views.py por este
 
-from django.contrib.auth.views import (
-
-    LoginView, LogoutView, PasswordResetView, 
-
-    PasswordResetDoneView, PasswordResetConfirmView
-
-)
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.messages.views import SuccessMessageMixin
-
 from django.urls import reverse_lazy
-
-from django.views.generic import CreateView
-
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth import login
-
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.shortcuts import render, redirect
+from django import forms
 
 from .models import User
 
-from django import forms
 
-class LoginForm(forms.Form):
+# ==================== FORMULÁRIOS ====================
 
+class LoginForm(AuthenticationForm):
+    """Formulário de login - HERDA DE AuthenticationForm (OBRIGATÓRIO!)."""
     username = forms.CharField(
-
         widget=forms.TextInput(attrs={
-
-            'class': 'form-control',
-
-            'placeholder': 'Nome de usuário'
-
+            'class': 'form-control form-control-lg',
+            'placeholder': 'Digite seu nome de usuário',
+            'autofocus': True
         })
-
     )
-
     password = forms.CharField(
-
         widget=forms.PasswordInput(attrs={
-
-            'class': 'form-control',
-
-            'placeholder': 'Senha'
-
+            'class': 'form-control form-control-lg',
+            'placeholder': 'Digite sua senha'
         })
-
     )
 
- 
-
- 
-
-class CustomLoginView(SuccessMessageMixin, LoginView):
-
-    template_name = 'accounts/login.html'
-
-    form_class = LoginForm
-
-    redirect_authenticated_user = True
-
-    success_message = 'Bem-vindo(a), %(username)s!'
-
-    
-
-    def get_success_url(self):
-
-        return reverse_lazy('tickets:dashboard')
-
- 
-
- 
-
-class CustomLogoutView(LogoutView):
-
-    next_page = reverse_lazy('accounts:login')
-
- 
-
- 
 
 class UserRegistrationForm(forms.ModelForm):
-
+    """Formulário de registro de novo usuário."""
     password1 = forms.CharField(
-
         label='Senha',
-
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
-
     )
-
     password2 = forms.CharField(
-
         label='Confirmar Senha',
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
-
     )
 
-    
-
     class Meta:
-
         model = User
-
-        fields = ['username', 'email', 'first_name', 'last_name', 
-
-                  'departamento', 'telefone']
-
+        fields = ['username', 'email', 'first_name', 'last_name', 'departamento', 'telefone']
         widgets = {
-
             'username': forms.TextInput(attrs={'class': 'form-control'}),
-
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
-
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-
             'departamento': forms.TextInput(attrs={'class': 'form-control'}),
-
             'telefone': forms.TextInput(attrs={'class': 'form-control'}),
-
         }
 
-    
-
     def clean_password2(self):
-
         password1 = self.cleaned_data.get('password1')
-
         password2 = self.cleaned_data.get('password2')
-
         if password1 and password2 and password1 != password2:
-
             raise forms.ValidationError('As senhas não conferem.')
-
         return password2
 
-    
-
     def save(self, commit=True):
-
         user = super().save(commit=False)
-
         user.set_password(self.cleaned_data['password1'])
-
         if commit:
-
             user.save()
-
         return user
 
- 
 
- 
+class ProfileUpdateForm(forms.ModelForm):
+    """Formulário para atualização de perfil."""
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'departamento', 'telefone']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'departamento': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
+# ==================== VIEWS ====================
+
+class CustomLoginView(SuccessMessageMixin, LoginView):
+    """View de login personalizada."""
+    template_name = 'accounts/login.html'
+    form_class = LoginForm
+    redirect_authenticated_user = True
+    success_message = 'Bem-vindo(a), %(username)s)!'
+
+    def get_success_url(self):
+        return reverse_lazy('tickets:dashboard')
+
+
+class CustomLogoutView(LogoutView):
+    """View de logout - redireciona para login."""
+    next_page = reverse_lazy('accounts:login')
+
 
 class RegisterView(SuccessMessageMixin, CreateView):
-
+    """View para registro de novos usuários."""
     model = User
-
     form_class = UserRegistrationForm
-
     template_name = 'accounts/register.html'
-
     success_url = reverse_lazy('tickets:dashboard')
-
     success_message = 'Conta criada com sucesso! Você está logado.'
 
     def form_valid(self, form):
-
         response = super().form_valid(form)
-
         login(self.request, self.object)
-
         return response
-    
-    # accounts/views.py (adicionar ao final do arquivo existente)
 
- 
-
-from django.contrib.auth.decorators import login_required
-
-from django.views.generic import UpdateView
-
-from django.urls import reverse_lazy
-
-from django.contrib import messages
-
-from django.contrib.auth.forms import PasswordChangeForm
-
-from django.contrib.auth import update_session_auth_hash
-
- 
 
 class ProfileUpdateView(SuccessMessageMixin, UpdateView):
-
+    """View para atualização de perfil."""
     model = User
-
-    fields = ['first_name', 'last_name', 'email', 'departamento', 'telefone']
-
+    form_class = ProfileUpdateForm
     template_name = 'accounts/profile.html'
-
     success_url = reverse_lazy('accounts:profile')
-
     success_message = 'Perfil atualizado com sucesso!'
 
-    
-
     def get_object(self):
-
         return self.request.user
-    
+
+
 @login_required
-
 def alterar_senha(request):
-
     """View para alteração de senha."""
-
     if request.method == 'POST':
-
         form = PasswordChangeForm(request.user, request.POST)
-
         if form.is_valid():
-
             user = form.save()
-
             update_session_auth_hash(request, user)
-
             messages.success(request, 'Senha alterada com sucesso!')
-
             return redirect('accounts:profile')
-
     else:
-
         form = PasswordChangeForm(request.user)
-
-    
 
     return render(request, 'accounts/alterar_senha.html', {'form': form})
