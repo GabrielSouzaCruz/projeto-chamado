d80e4610d08cbb0c43dd04c4e8c67a78439ab77f
GabrielSouzaCruz <contato.gabrielsouzacruz@gmail.com>
2026-08-04 17:42:49 -0300

    feat: update Redis URL in channel layers configuration for secure connections

--- config/settings.py
+++ config/settings.py
@@ -223,7 +223,12 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')],
+            "hosts": [
+                os.environ.get(
+                    'REDIS_URL', 
+                    'rediss://default:gQAAAAAAAe7qAAIgcDI2ZDgxMDRjYmUwODk0YjBjYmJkZmE1NGU3YWYzYjhmYQ@capital-bat-126698.upstash.io:6379'
+                )
+            ],
         },
     },
 }
