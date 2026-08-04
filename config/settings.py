9f1dc67d62a3231a2927e16a86141337ffac16cf
GabrielSouzaCruz <contato.gabrielsouzacruz@gmail.com>
2026-08-04 17:55:34 -0300

    feat: update timezone to America/Sao_Paulo and add websocket connection for real-time notifications

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Force UTC para evitar anomalias de fuso horário
 LANGUAGE_CODE = 'pt-br'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
