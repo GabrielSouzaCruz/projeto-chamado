4a424d74d59e3316b510dfa3779d8ee32493471a
GabrielSouzaCruz <contato.gabrielsouzacruz@gmail.com>
2026-08-04 17:00:41 -0300

    feat: update channel layers to use Redis backend for improved performance

--- config/settings.py
+++ config/settings.py
@@ -220,8 +220,11 @@ DEFAULT_FROM_EMAIL = 'sistema@localhost'
 # =============================================================================
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')],
+        },
+    },
 }
 
 # =============================================================================
